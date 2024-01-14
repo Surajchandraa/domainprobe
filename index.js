3a6de5578f59329139aa74a5577705002566fac1
suraj <surajjosh7@gmail.com>
2024-01-14 20:13:02 +0530

    updated

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const get_hostname=require('./src/other_data');
 const data_extract_ip=require('./src/data_direct_by_ip');
 const exact_location=require('./src/location');
 const get_text=require('./src/txtrecord');
+const {get_cname,get_mx}=require("./src/mx_cname_records")
 
 
 
 
 
-module.exports={get_ip,data_extract,get_hostname,data_extract_ip,exact_location,get_text}
+module.exports={get_ip,data_extract,get_hostname,data_extract_ip,exact_location,get_text,get_cname,get_mx}
